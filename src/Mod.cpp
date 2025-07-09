98d21ad0d60f341b705ab5ee706fa4021c50c3c8
AmeliaCute <48628282+AmeliaCute@users.noreply.github.com>
2025-07-09 12:19:31 +0100

    Adding documentation

--- src/Mod.cpp
+++ src/Mod.cpp
@@ -5,15 +5,26 @@ class MyMod : public ModBase
     Logger* logger;
 
     public:
+
+    // Called when the game start
     void OnPreLoad() override
     {
+        // Creating your own logger
         logger = new Logger("MyMod");
         logger->info("Hellooooo");
 
+        // Retrieve the Faraway Iron item from the ModLoader cache
         ItemData item = ModLoader::gameCache->GetItem(MATERIAL_FARAWAY_IRON);
+
+        // Print the item's english name
         logger->info(item.GetName(LANG::ENGLISH));
+
+        // Modify item properties
+        item.SetName(LANG::ENGLISH, L"MODIFIED BY MOD");
+        item.SetQuality(EItemQualityType::EItemQualityType__Quality4);
     }
 
+    // Called when the game loads the save file
     void OnPostLoad() override
     {
         logger->info("Hellooooo 2");
